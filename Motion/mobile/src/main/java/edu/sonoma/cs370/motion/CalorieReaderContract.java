6b807d150714971234f024aaec85f596839cc341
johncox515 <coxj@sonoma.edu>
2015-11-09 21:26:35 -0800

    Made some minor edits to SQLlite classes

--- Motion/mobile/src/main/java/edu/sonoma/cs370/motion/CalorieReaderContract.java
+++ Motion/mobile/src/main/java/edu/sonoma/cs370/motion/CalorieReaderContract.java
@@ -27,9 +27,11 @@ public final class CalorieReaderContract {
         public CalorieReaderDbHelper(Context context){
             super(context, DATABASE_NAME, null, DATABASE_VERSION);
         }
+        @Override
         public void onCreate(SQLiteDatabase db){
             db.execSQL(SQL_CREATE_ENTRIES);
         }
+        @Override
         public void onUpgrade(SQLiteDatabase db, int OldVersion, int newVersion){
             //This database is only a cache for online data, so its upgrade
             // policy is to simply to discard the data and start over.
@@ -41,15 +43,14 @@ public final class CalorieReaderContract {
         }
 
         private static final String TEXT_TYPE_INT = " INT";
-        private static final String TEXT_TYPE_TEXT = " VARCHAR(50)";
+        private static final String TEXT_TYPE_TEXT = " TEXT";
         private static final String COMMA_SEP = ",";
         private static final String SQL_CREATE_ENTRIES =
                 "CREATE TABLE " + CalorieEntry.TABLE_NAME + " (" +
                  CalorieEntry._ID + " INTEGER PRIMARY KEY AUTO_INCREMENT," +
                  CalorieEntry.COLUMN_NAME_CALORIE_START + TEXT_TYPE_INT + COMMA_SEP +
                  CalorieEntry.COLUMN_NAME_CURRENT_CALORIES + TEXT_TYPE_INT + COMMA_SEP +
-                 CalorieEntry.COLUMN_NAME_DATE + TEXT_TYPE_TEXT + COMMA_SEP +
-                 " )";
+                 CalorieEntry.COLUMN_NAME_DATE + TEXT_TYPE_TEXT + " );";
 
         private static final String SQL_DELETE_ENTRIES =
                 "DROP TABLE IF EXISTS " + CalorieEntry.TABLE_NAME;
